6c969ca92b3155385b7fa3dfb26f6956437149bb
Paul Raffer <znpivzcx@anonaddy.me>
2021-05-30 20:55:09 +0200

    raffer -> gpl

--- number_theory.hpp
+++ number_theory.hpp
@@ -1,48 +1,64 @@
 
-// Copyright (c) 2020 Paul Raffer.
+// Copyright Paul Raffer 2020.
 // Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
+// (See accompanying file LICENSE or copy at
 // https://www.boost.org/LICENSE_1_0.txt)
 
 #if __cplusplus >= 201402L
 
-#ifndef RAFFER_NUMBER_THEORY_NUMBER_THEORY_HPP
-#define RAFFER_NUMBER_THEORY_NUMBER_THEORY_HPP
+#ifndef GPL_NUMBER_THEORY_NUMBER_THEORY_HPP
+#define GPL_NUMBER_THEORY_NUMBER_THEORY_HPP
 
-#define raffer_cpplib_number_theory__          202007L
-#define raffer_cpplib_number_theory_odd_even__ 202005L
-#define raffer_cpplib_number_theory_prime__    202007L
-#define raffer_cpplib_number_theory_digit__    202006L
+#define gpl_cpp_number_theory__          202007L
+#define gpl_cpp_number_theory_odd_even__ 202005L
+#define gpl_cpp_number_theory_prime__    202007L
+#define gpl_cpp_number_theory_digit__    202006L
 
 
 #include <cmath>
 
 
-namespace raffer // interface
+namespace gpl { // interface
+
+template <typename T>
+constexpr auto is_odd(T x) -> bool;
+
+template <typename T>
+constexpr auto is_even(T x) -> bool;
+
+
+template <typename T>
+constexpr auto is_prime(T x) -> bool;
+
+template <typename T>
+constexpr auto prime(T x) -> int;
+
+
+template <typename T>
+auto digit(T x, int d, int n = 1, int base = 10) -> int;
+
+} // namespace gpl
+
+
+
+
+
+namespace gpl { // implementation
+
+template <typename T>
+constexpr auto is_odd(T x) -> bool
 {
+	return x & 1;
+}
 
-template <typename T> constexpr auto is_odd(T x) -> bool;
-template <typename T> constexpr auto is_even(T x) -> bool;
-
-template <typename T> constexpr auto is_prime(T x) -> bool;
-template <typename T> constexpr auto prime(T x) -> int;
-
-
-template <typename T> auto digit(T x, int d, int n = 1, int base = 10) -> int;
-
-} // namespace raffer
-
-
-
-
-
-namespace raffer // implementation
+template <typename T>
+constexpr auto is_even(T x) -> bool
 {
+	return !is_odd(x);
+}
 
-template <typename T> constexpr auto is_odd(T x) -> bool { return x & 1; }
-template <typename T> constexpr auto is_even(T x) -> bool { return !is_odd(x); }
-
-template <typename T> constexpr auto is_prime(T x) -> bool
+template <typename T>
+constexpr auto is_prime(T x) -> bool
 {
 	for (auto i = 3; i <= sqrt(x); i += 2)
 		if (x % i == 0)
@@ -68,13 +84,13 @@ template <typename T>
 auto digit(T x, int d, int n, int base) -> int
 {
 	return
-		static_cast<int>(x / std::pow(base, d))
-		% static_cast<int>(std::pow(base, n));
+			static_cast<int>(x / std::pow(base, d)) %
+			static_cast<int>(std::pow(base, n));
 }
 
-} // namespace raffer
+} // namespace gpl
 
 
-#endif // RAFFER_NUMBER_THEORY_NUMBER_THEORY_HPP
+#endif // GPL_NUMBER_THEORY_NUMBER_THEORY_HPP
 
 #endif // __cplusplus >= 201402L
