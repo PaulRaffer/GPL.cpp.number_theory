858efd13ef459691e60591dab2364c5d59cd480c
Paul Raffer <znpivzcx@anonaddy.me>
2021-05-30 21:05:35 +0200

    Edit header

--- number_theory.hpp
+++ number_theory.hpp
@@ -1,7 +1,7 @@
 
-// Copyright (c) 2020 Paul Raffer.
+// Copyright Paul Raffer 2020.
 // Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
+// (See accompanying file LICENSE or copy at
 // https://www.boost.org/LICENSE_1_0.txt)
 
 #if __cplusplus >= 202002L
